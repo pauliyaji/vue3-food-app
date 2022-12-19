cb7002571575c2d46e49d57290267b026d7f2bdc
Paul-Iyaji <pauliyaji@gmail.com>
2022-12-19 16:20:37 +0000

    update on homepage and index file

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,26 @@
 
 <template>
 
-    <h2>Hello from Paul</h2>
+   
+<div class="max-w p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-purple-900 dark:text-white">Simple CRUD using Laravel 9 API and VueJS 3</h5>
+    </a>
+   
+<ul class="w-md text-lg font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+    <li class="py-2 px-4 w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">Laravel 9 API</li>
+    <li class="py-2 px-4 w-full border-b border-gray-200 dark:border-gray-600">Vue 3</li>
+    <li class="py-2 px-4 w-full border-b border-gray-200 dark:border-gray-600">Tailwind CSS</li>
+    <li class="py-2 px-4 w-full border-b border-gray-200 dark:border-gray-600">Sweetalert 2</li>
+    <li class="py-2 px-4 w-full border-b rounded-b-lg">Github</li>
+    <li class="py-2 px-4 w-full rounded-b-lg">Continuous Integration/Continous Delivery (CI/CD)</li>
+</ul>
+<br />
+<a href="https://github.com/pauliyaji/vue3-food-app-crud.git" target="_blank" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-purple-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+    Click to download the full codes from github 
+    <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+</a>
+</div>
+
 
 </template>
