d3a7aa424dd644ddec5df758959d50d1c2ef312f
Paul-Iyaji <pauliyaji@gmail.com>
2022-12-19 16:28:24 +0000

    spacing to buttons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -19,11 +19,11 @@
     <li class="py-2 px-4 w-full rounded-b-lg">Continuous Integration/Continous Delivery (CI/CD)</li>
 </ul>
 <br />
-<a href="https://github.com/pauliyaji/vue3-food-app-crud.git" target="_blank" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-purple-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+<a href="https://github.com/pauliyaji/vue3-food-app-crud.git" target="_blank" class="inline-flex items-center px-3 py-2 text-sm m-2 font-medium text-center text-white bg-purple-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
     Click to download the Vue 3 full codes from github 
     <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
 </a>
-<a href="https://github.com/pauliyaji/lara9-crud_with_API.git" target="_blank" class="float-right inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-500 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+<a href="https://github.com/pauliyaji/lara9-crud_with_API.git" target="_blank" class="float-right inline-flex items-center px-3 py-2 m-2 text-sm font-medium text-center text-white bg-green-500 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
     Click to download the Laravel 9 API full codes from github 
     <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
 </a>
